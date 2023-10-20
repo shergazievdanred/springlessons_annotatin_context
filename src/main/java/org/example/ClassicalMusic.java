92ae93036bc6697aae3740d157f930d58343c92f
user <shergazievdan@gmail.com>
2023-10-20 11:14:25 +0600

    third commit

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
-//@Scope("prototype")
-
 public class ClassicalMusic implements Music {
     @PostConstruct
     public void doMyInit() {
--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -2,12 +2,7 @@ package org.example;
 
 import org.springframework.stereotype.Component;
 
-@Component
 public class RockMusic implements Music {
-    private RockMusic() {
-    }
-
-
     @Override
     public String getSong() {
         return "Points of authority";
